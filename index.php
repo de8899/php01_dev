864451ad7c528b6e7af4379ce0b3f45facf924fa
derwin8899 <de8899@gmail.com>
2021-02-06 20:51:32 -0800

    updates

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ p {
 
 <p>
 The code for this page is pulled directly from a github repo.
-New commits to the repo are automatically pulled to update the site within 1 minute. 
+New commits to the repo are automatically deployed to update the site within 1 minute. 
 Link to Github repo
 </p>
 <a href="https://github.com/de8899/php01_dev/index.php">Link to Github repo</a>
@@ -31,29 +31,25 @@ Link to Github repo
 <?php
 
 // pre enables all of the output to be captured if muliple lines.
-
 //notice that the star and fwd slash is for multiline comments.
-
 /* try to display some OS stuff obtained via bash cmds.
  we are adding p tags to get the fomatting from the css block above.
 */
 
-
-
 $cmd1 = 'uptime';
 $out1 = "<pre>".shell_exec($cmd1)."</pre>";
-echo "<p>"."The current uptime is: $out1"."</p>";
-echo "<br >";
+echo "<p>"."Server uptime info: $out1"."</p>";
+//echo "<br />";
 
 $cmd2 = 'lscpu | grep \'Model name\'';
 $out2= "<pre>".shell_exec("$cmd2")."</pre>";
 echo "<p>"."CPU: $out2"."</p>";
-echo "<br />";
+//echo "<br />";
 
 $cmd3 = 'lscpu | grep \'MHz\'';
 $out3 =  "<pre>".shell_exec($cmd3)."</pre>";
 echo "<p>"."CPU Speed: $out3"."</p>";
-echo "<br />";
+//echo "<br />";
 
 $cmd4 = 'cat /etc/resolv.conf';
 $out4 = "<pre>".shell_exec($cmd4)."</pre>";
